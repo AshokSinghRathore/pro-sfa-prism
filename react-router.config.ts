d47126cbf9947f7dd2712cd00653b0664f8bea06
Satvickk <lenovo37819@gmail.com>
2025-06-06 17:16:22 +0530

    transferred from ssr to static building process

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from "@react-router/dev/config";
+// react-router.config.ts
+import type { Config } from '@react-router/dev/config';
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  appDirectory: 'app',  // your current app folder
+  ssr: false,           // tell the plugin to not generate server code
 } satisfies Config;
