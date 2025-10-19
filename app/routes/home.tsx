dc7a9092fab8b4595f4c377196cee6350e7b5beb
AshokSinghRathore <147539487+AshokSinghRathore@users.noreply.github.com>
2025-10-20 04:34:04 +0530

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,17 +9,18 @@ import { Footer } from "components/common/footer";
 import ContactForm from "components/contact/contactForm";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    {
-      title:
-        "Prism SFA - Field Force Management & Pharma Sales Automation Software",
-    },
-    {
-      name: "description",
-      content:
-        "Prism SFA is a powerful field force management software and mobile CRM for field teams, offering field sales automation, medical rep order management, GPS tracking, and real-time analytics for pharmaceutical sales.",
-    },
-  ];
+  return[
+  {
+    title:
+      "Bizzfield - Field Force Management & Pharma Sales Automation Software",
+  },
+  {
+    name: "description",
+    content:
+      "Bizzfield is a powerful field force management software and mobile CRM for field teams, offering field sales automation, medical rep order management, GPS tracking, and real-time analytics for pharmaceutical sales.",
+  },
+];
+
 }
 
 export default function Home() {
